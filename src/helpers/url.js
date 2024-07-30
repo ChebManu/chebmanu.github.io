d8b3afcca318107076ef8e01f9ba9ee94dd4062b
ChebManu <165665464+ChebManu@users.noreply.github.com>
2024-07-30 14:59:52 +0200

    move to build dir

--- src/helpers/url.js
+++
@@ -1,8 +0,0 @@
-/**
- * Opens a new tab with the given url
- * @param {string} url
- */
-export function openTab(url) {
-  const win = window.open(url, '_blank');
-  win.focus();
-}
--- src/mui/colors.js
+++
@@ -1,3 +0,0 @@
-export const ASTIROIDS_BLACK = '#000000';
-export const ASTIROIDS_WHITE = '#FFFFFF';
-export const ASTIROIDS_GRAY = '#1b1b1b';
